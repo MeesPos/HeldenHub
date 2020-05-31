b9edf4e6ce1c85ea98c30f31f66f0a963373cdc9
Mees Postma <35431488+MeesPos@users.noreply.github.com>
2020-05-31 20:23:26 +0200

    Update model.php

--- private/models/model.php
+++ private/models/model.php
@@ -8,6 +8,19 @@ function getUsers(){
 
 }
 
+function alleDetails()
+{
+    $connection = dbConnect();
+    $sql = 'SELECT * 
+    FROM `gebruikers`
+    INNER JOIN `posts` 
+    ON `posts`.`gebruiker_id` = `gebruikers`.`id`
+    WHERE `posts`.`gebruiker_id` = 1';
+    $statement = $connection->query($sql);
+
+    return $statement->fetchAll();
+}
+
 function getUsersByEmail($email){
     $connection = dbConnect();
 	$sql =  'SELECT * FROM `gebruikers` WHERE `email`= :email';
@@ -32,4 +45,61 @@ function getUsersById($id){
 
 
 return false;
+}
+
+function alleDetailsContact()
+{
+    $connection = dbConnect();
+    $sql = 'SELECT * 
+    FROM `gebruikers`
+    INNER JOIN `posts` 
+    ON `posts`.`gebruiker_id` = `gebruikers`.`id`
+    WHERE `posts`.`gebruiker_id` = 1';
+    $statement = $connection->query($sql);
+
+    return $statement->fetchAll();
+function getUserData() {
+    $connection = dbConnect();
+    $query      = 'SELECT * FROM `gebruikers` WHERE `id` = :gebruiker_id';
+    $statement  = $connection->prepare($query);
+
+    $params = [
+        'gebruiker_id' => 1
+    ];
+    
+    $statement->execute($params);
+    
+    return $statement->fetch();
+}
+
+function getUserData() {
+    $connection = dbConnect();
+    $query      = 'SELECT * FROM `gebruikers` WHERE `id` = :gebruiker_id';
+    $statement  = $connection->prepare($query);
+
+    $params = [
+        'gebruiker_id' => 1
+    ];
+    
+    $statement->execute($params);
+    
+    return $statement->fetch();
+}
+
+	function savePost() {
+    $connection = dbConnect();
+    $query      = 'INSERT INTO `posts` (`id`, `titel`, `inhoud`, `gebruiker_id`) VALUES (NULL, :titel, :inhoud,  :gebruiker_id)';
+ 
+    $statement  = $connection->prepare($query);
+
+    $params = [
+        'titel'         => $_POST['titel'],
+        'inhoud'        => $_POST['inhoud'],
+        'gebruiker_id'  => 1,
+    ];
+
+    $statement->execute($params);
+
+    $redirectURL = url('/bap/Heldenhub/public');
+	redirect($redirectURL);
 }
