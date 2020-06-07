0a2d356440cdb8f5e3ad6cd632a7044e9c08985a
MeesPos <meespostma_@hotmail.com>
2020-06-07 18:24:31 +0200

    Missende dingen toegevoegd

--- private/controllers/WebsiteController.php
+++ private/controllers/WebsiteController.php
@@ -82,7 +82,7 @@ class WebsiteController
 			echo $template_engine->render('bedanktPagina');
 			exit;
 		} else {
-			$errors['email'] = 'Dit accouny bestaat al!!!!!';
+			$errors['email'] = 'Dit account bestaat al!';
 
 		}
 
@@ -203,6 +203,47 @@ class WebsiteController
 
 		$template_engine = get_template_engine();
 		echo $template_engine->render('details', ['AlleDetails' => $details]);
+	}
+
+	public function detailsContact()
+	{
+		$id = $_POST['hiddenId'];
+		$naam = $_POST['naam'];
+		$email = $_POST['email'];
+		$bericht = $_POST['bericht'];
+
+		$connection = dbConnect();
+		$sql = 'SELECT * 
+    		FROM `gebruikers`
+    		INNER JOIN `posts` 
+    		ON `posts`.`gebruiker_id` = `gebruikers`.`id`
+			WHERE `posts`.`id` = :id';
+		$statement = $connection->prepare($sql);
+		$idQuery = [
+			'id' => $id
+		];
+		$statement->execute($idQuery);
+
+		$data = $statement->fetch();
+
+		$mailer = getSwiftMailer();
+
+		$message = createEmailMessage($data['email'], $naam . ' wilt u helpen!', $naam, '29035@ma-web.nl');
+		
+		$message->setBody('<html>' .
+		' <body> ' .
+		' <p> U kunt niet reagen op deze email, die wordt niet gelezen.</p>' .
+		' <p> Contact opnemen met degene die u wilt helpen? Neem contact op met: ' . '<b>' . $email . '</b>' .
+		' <p> Zijn/Haar bericht was: ' . $bericht . '</p>' .
+		' <p> Email van ' . '<b>' . $naam . '</b>' . ' is ' . '<b>' . $email . '</b>' .
+		' </body> ' .
+		' </html>',
+		'text/html' );
+
+		$aantalVerstuurd = $mailer->send($message);
+		
+		$bedanktUrl = url("bedanktContact");
+		redirect($bedanktUrl);
 	}
 
 	public function bevestigenEmail()
