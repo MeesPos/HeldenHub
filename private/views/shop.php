ea2ecf9c4e2064bd1cc9fd12d7cc798eefb8e068
MeesPos <meespostma_@hotmail.com>
2020-06-14 21:09:32 +0200

    Titel aangepast

--- private/views/shop.php
+++ private/views/shop.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hulp vragen</title>
+    <title>Shop</title>
     <link rel="stylesheet" href="<?php echo site_url('/css/style.css') ?>" media="all">
     <script src="https://kit.fontawesome.com/a82e000026.js"></script>
 </head>
